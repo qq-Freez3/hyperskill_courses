8ecebd432c86f39dd904d20ce4e1ca9f13ce34f7
qq-Freez3 <freez3labs@gmail.com>
2025-11-11 16:07:05 -0500

    updated pass attempt at: https://hyperskill.org/learn/step/15149

--- Regex Course/extra_curricular.py
+++ Regex Course/extra_curricular.py
@@ -3,13 +3,13 @@ import re
 def your_name_please():
     """FirstName MiddleName? Smith"""
 
-    arrow = "[A-Z][a-z]+\s[A-Z]?[a-z]*?\s?Smith"
+    arrow = "([A-Z][a-z]+)(\\s[A-Z][a-z]+)?\\sSmith"
     hit = lambda target: print(re.match(arrow, target))
 
-    hit("Chaz Smith")
-    hit("Rhonda Rousey Smith")
-    hit("smith Smith")
-    hit("Dusty GabbaSmith") #fail, retry
+    hit("Chaz Smith") # pass successfully
+    hit("Rhonda Rousey Smith") #pass successfully
+    hit("smith Smith") #fail successfully
+    hit("Dusty GabbaSmith") #fail successfully
 
 
 if __name__ == '__main__':
