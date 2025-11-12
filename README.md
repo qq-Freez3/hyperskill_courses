82cd4dc6b7a1b1591cbfb591f16b248ea1bf4e4c
qq-Freez3 <freez3labs@gmail.com>
2025-11-12 10:57:32 -0500

    update readME, added numbered_list() assignment to extra_curricular module

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 I studied Computer Science for 4 Years in college but never used it;
 Re-Learning how to code through JetBrains Academy's Python Developer Roadmap;
 Link: https://academy.jetbrains.com/?tag=Python&mode=roadmap
+
+Useful Links:
+https://docs.python.org/3/howto/regex.html#grouping
--- Regex Course/extra_curricular.py
+++ Regex Course/extra_curricular.py
@@ -3,7 +3,7 @@ import re
 def your_name_please():
     """FirstName MiddleName? Smith"""
 
-    arrow = "([A-Z][a-z]+)(\\s[A-Z][a-z]+)?\\sSmith"
+    arrow = r"([A-Z][a-z]+)(\s[A-Z][a-z]+)?\sSmith"
     hit = lambda target: print(re.match(arrow, target))
 
     hit("Chaz Smith") # pass successfully
@@ -11,6 +11,15 @@ def your_name_please():
     hit("smith Smith") #fail successfully
     hit("Dusty GabbaSmith") #fail successfully
 
+def numbered_list():
+    """ 1) Curiosity 2) Perseverance 3) ?
+        1) cabbage 2) carrot 3) apple 4) oranges """
+
+    arrow = r"^([0-9]+\)\s{1}[A-Za-z]+\s{1})*?([0-9]+\)\s{1}[A-Za-z]+)$"
+    target = "1) cabbage 2) carrot 3) apple 4) oranges"
+    hit = lambda x: print(re.match(arrow, x))
+    hit(target)
 
 if __name__ == '__main__':
-    your_name_please() #quantifiers module
+    #your_name_please() #quantifiers module
+    numbered_list() #groups and alternations module
